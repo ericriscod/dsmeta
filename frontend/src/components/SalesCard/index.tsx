fc70a341dedee944af4dea0720dfb0bf5253a486
Ericris Rossato <rossato.ep@gmail.com>
2022-07-17 19:26:33 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,10 +19,14 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([])
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(responde => {
+
+        const dmin = minDate.toISOString().slice(0,10);
+        const dmax = maxDate.toISOString().slice(0,10);
+        
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(responde => {
             setSales(responde.data.content);
         })
-    }, []);
+    }, [minDate, maxDate]);
 
 
     return (
